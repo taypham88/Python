09f83ab465c4b022874a9323e863499dcdaaf813
taypham88 <92409737+taypham88@users.noreply.github.com>
2023-05-17 21:57:43 -0700

    cont.

---
+++ Python_330A/Assign06_blog_proj/site/polling/urls.py
@@ -0,0 +1,9 @@
+# polling/urls.py
+
+from django.urls import path
+from polling.views import PollListView, PollDetailView
+
+urlpatterns = [
+    path("", PollListView.as_view(), name="poll_index"),
+    path("polls/<int:pk>/", PollDetailView.as_view(), name="poll_detail"),
+]
